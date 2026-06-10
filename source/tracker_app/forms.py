75c283a81fbb4b244b1e442fda278e4323567bb4
Kanzhar Ryskulov <ryskulovkanzar@gmail.com>
2026-06-10 23:48:35 +0600

    Из-за ошибки пришлось изменить положение файлы .git

---
+++ source/tracker_app/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from tracker_app.models import Task, Type, Status
+
+
+class TaskForm(forms.ModelForm):
+    summary = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
+    status = forms.ModelChoiceField(queryset=Status.objects.all(), widget=forms.Select(attrs={'class': 'form-control'}))
+    type = forms.ModelChoiceField(queryset=Type.objects.all(), widget=forms.Select(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = Task
+        fields = '__all__'
+        exclude = ('created_at', 'updated_at')
+
+class TypeForm(forms.ModelForm):
+    class Meta:
+        model = Type
+        fields = '__all__'
+
+class StatusForm(forms.ModelForm):
+    class Meta:
+        model = Status
+        fields = '__all__'
